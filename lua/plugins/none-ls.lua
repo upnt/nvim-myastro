f12624d0c5f65abbbf059dcc47dde614fca36366
upnt <upnt.github@gmail.com>
2024-09-02 04:09:25 +0900

    support virtual env

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -17,6 +17,12 @@ return {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettier,
+      null_ls.builtins.diagnostics.mypy.with {
+        extra_args = function()
+          local virtual = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX" or "/usr"
+          return { "--python-executable", virtual .. "/bin/python3" }
+        end,
+      },
     })
   end,
 }
