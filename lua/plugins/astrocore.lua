7ca9be1999901cf7875fc617186c1e7ebd1322af
upnt <upnt.github@gmail.com>
2024-10-28 03:21:54 +0900

    add fileencodings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -16,6 +16,8 @@ return {
       diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
       highlighturl = true, -- highlight URLs at start
       notifications = true, -- enable notifications at start
+      fileencodings = "ucs-bom,utf-8,iso-2022-jp,sjis,cp936,cp932,gb18030,big5,enc-jp,euc-kr,default,latin1,latin",
+      fileencoding = "utf-8",
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
